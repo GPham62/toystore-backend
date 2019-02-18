e2072d68c749894c1a9b98ca5117fe12e3abdc86
TuanAnhPham <ptuananh196@gmail.com>
2019-02-18 18:13:17 +0700

    .

--- modules/api/auth/router.js
+++ modules/api/auth/router.js
@@ -5,8 +5,8 @@ const authController = require("./controller");
 
 router.get("/", (req,res) =>{
   const {userInfo} = req.session;
+  console.log(req.session);
   if (userInfo && userInfo.username){
-    console.log(req.session);
     res.json(userInfo);
   } else{
     res.status(401).json("Not logging in");
